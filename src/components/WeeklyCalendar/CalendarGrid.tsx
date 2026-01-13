65e371151fead1b04ec10cd7e347b468d34d27a6
Giannis <gvar95@gmail.com>
2026-01-13 22:04:48 +0200

    added rows to the calendar grid

--- src/components/WeeklyCalendar/CalendarGrid.tsx
+++ src/components/WeeklyCalendar/CalendarGrid.tsx
@@ -5,26 +5,38 @@ import { getCurrentDay, isToday } from "./date.utils"
 type Props = { week: CalendarWeek }
 
 const CalendarGrid = ({ week }: Props) => {
+
+    const hours = Array.from({ length: 17 }, (_, i) => 17 + i)
+
     return (
         <div style={{
             display: "grid",
             gridTemplateColumns: "repeat(7, 1fr)",
-            gap: 0
+            gap: 0,
+            overflowY: "scroll",
+            height: "100vh"
         }}>
             {week.days.map(day => (
                 <div
                     key={day.isoDate}
                     style={{
                         border: "1px solid #ddd",
-                        padding: 10,
                         minHeight: 220,
                     }}
                 >
                     <div style={{ fontWeight: 700, marginBottom: 8, textAlign: "center" }}>{day.date.getDate()}</div>
                     <div style={{ fontWeight: 700, marginBottom: 8, textAlign: "center" }}>{getCurrentDay(day.date.getDay())}</div>
 
+                    {hours.map((hour) =>
+                        <div key={`${day.isoDate}-${hour}`} style={{ border: "1px solid #ddd" }}>
+                            <div style={{ display: "grid", gridTemplateRows: `repeat(${hours.length}, 4px)` }}>
 
-                    <div style={{ display: "flex", flexDirection: "column" }}>
+                            </div>
+                        </div>
+                    )}
+
+
+                    {/*<div style={{ display: "flex", flexDirection: "column" }}>
                         {day.tasks.length === 0 ? (
                             <div style={{ opacity: 0.5, fontSize: 12 }}> No tasks </div>
                         ) : (
@@ -39,7 +51,7 @@ const CalendarGrid = ({ week }: Props) => {
                                 </div>
                             ))
                         )}
-                    </div>
+                    </div>*/}
 
                 </div>
             ))}
